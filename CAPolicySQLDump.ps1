4e6a324fd314aa767364c93682fcb529accc922e
Joe Garcia, CISSP <joe.garcia@cyberark.com>
2017-08-01 12:36:55 -0400

    Added Pre-Reqs, Support, License

--- CAPolicySQLDump.ps1
+++ CAPolicySQLDump.ps1
@@ -1,3 +1,30 @@
 #############################
 #### CAPolicySQLDump.ps1 ####
 #############################
+
+Import-Module PoShPACLI
+
+# VARIABLES (CHANGE HERE)
+$pacliFolder = "C:\PACLI"
+$vaultName = "VAULT"
+$vaultAddress = "192.168.2.150"
+$username = "Administrator"
+$userIni = "C:\PACLI\user.ini"
+$safe = "PasswordManager_info"
+
+# EXECUTE
+Initialize-PoShPACLI -pacliFolder $pacliFolder
+
+Start-PACLI
+
+Add-VaultDefinition -vault $vaultName -address $vaultAddress
+
+Connect-Vault -vault $vaultName -user $username -logonFile $userIni
+
+Open-Safe -vault $vaultName -user $username -safe $safe
+
+$files = Get-FilesList -vault $vaultName -user $username -safe $safe -folder "Root"
+
+foreach ($file in $files) {
+    Write-Host $file
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # CAPolicySQLDump
+Takes PolicyID and PolicyName from every file in the PasswordManager_info safe and INSERT INTO capolicies table in mssql to cols caPolicyID and caPlatformName.
+
+## Pre-Requisites
+* This PowerShell script is unsigned and may throw warnings from PowerShell.  To prevent this, run `Set-ExecutionPolicy Unrestricted` in an elevated PowerShell console window.
+* `git clone https://github.com/psPete/PoShPACLI.git` into `C:\Windows\system32\WindowsPowerShell\v1.0\Modules`
+* Install CyberArk PACLI into `C:\PACLI` on the local machine this script is run from.
+
+## Support
+Please update the [Issues](https://github.com/infamousjoeg/CAPolicySQLDump/issues) on this repo for support.
+
+## License
+MIT
