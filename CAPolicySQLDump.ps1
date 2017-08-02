39f8c28e639f184cc6904c1d9705e6af5f438e80
Joe Garcia, CISSP <joe.garcia@cyberark.com>
2017-08-01 21:21:11 -0400

    Changed filename input method, added CSV, updated README

--- CAPolicySQLDump.ps1
+++ CAPolicySQLDump.ps1
@@ -11,6 +11,7 @@ $vaultAddress = "192.168.2.150"
 $username = "Administrator"
 $userIni = "C:\PACLI\user.ini"
 $safe = "PasswordManager_info"
+$policyCSV = "C:\PACLI\policy.csv"
 
 # EXECUTE
 Initialize-PoShPACLI -pacliFolder $pacliFolder
@@ -23,8 +24,13 @@ Connect-Vault -vault $vaultName -user $username -logonFile $userIni
 
 Open-Safe -vault $vaultName -user $username -safe $safe
 
-$files = Get-FilesList -vault $vaultName -user $username -safe $safe -folder "Root"
+$csvFile = Import-CSV -Path $policyCSV
 
-foreach ($file in $files) {
-    Write-Host $file
+foreach ($row in $csvFile) {
+    Get-File -vault $vaultName -user $username -safe $safe -folder Root\Policies -file $row.PolicyFileName -localFolder $env:TEMP -localFile "${row.PolicyFileName}.tmp" -evenIfLocked
+    $fileReceived = Get-Content "${env:TEMP}/${row.PolicyFileName}.tmp"
+    $PolicyID = $fileReceived | Select-String -Pattern "PolicyID=" -Encoding ascii | Select-Object -Last 1
+    $PolicyName = $fileReceived | Select-String -Pattern "PolicyName=" -Encoding ascii | Select-Object -Last 1
+    Write-Host "PolicyID:   ${PolicyID}"
+    Write-Host "PolicyName: ${PolicyName}"
 }
